74b93b235864cb2c97b0c322c8fc303400410a33
AngeloAG <angelo.ag.079@gmail.com>
2023-01-05 16:28:59 -0700

    Activity Prep 1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,24 @@
 using System;
+/*
+Author: Alberto Angelo Arellano Gaona
+Date: 1/05/2022
+Description:
+An iconic line from the James Bond movies is that he would introduce himself as "Bond, James Bond." For this assignment you will write a program that asks for your name and repeats it back in this way.
+*/
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Welcome agent.");
+    Console.Write("What is your first name? ");
+    string aaagFirstName = Console.ReadLine();
+    Console.WriteLine();
+    Console.Write("What is your last name? ");
+    string aaagLastName = Console.ReadLine();
+    Console.WriteLine();
+    Console.WriteLine($"Your name is {aaagLastName}, {aaagFirstName} {aaagLastName}.");
+
+
+  }
 }
